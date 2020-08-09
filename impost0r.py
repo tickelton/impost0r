7aee80c40d9fc4a404c5c01b891901fcb0b638e5
tickelton <tickelton@gmail.com>
2020-08-09 21:28:46 +0200

    Repo creation and push kinda working

--- impost0r.py
+++ impost0r.py
@@ -7,12 +7,15 @@ import time
 import dulwich
 import urllib.request
 import re
+from dulwich import porcelain
 
 # global variables
 user_name = 'XXXX'
-repo_name = 'decoy'
+user_pass = 'YYYY'
+repo_name = 'impost0r-demo'
+data_file = 'data.py'
 user_email = 'tickelton@gmail.com'
-donor_name = 'YYYY'
+donor_name = 'ZZZZ'
 
 
 # logging configuration
@@ -70,6 +73,14 @@ def diff_contribution_data(data_user, data_donor):
         data_diff[cdate] = count_donor - count_user
 
     #print(data_diff)
+    # TODO(possibly): add scaling
+    #  if the user calendar has days with more commits than the
+    #  donor calendar the merged calendar will look very different
+    #  to the donor calendar.
+    #  BEWARE: when scaling the data take care not to introduce
+    #    runaway upscaling effects when the function is called
+    #    repeatedly to update the user calendar when there are
+    #    new contributions in the donor calendar.
     return data_diff
 
 
@@ -79,14 +90,30 @@ def main():
     logger.debug('Using tempdir=%s', tempdir.name)
 
     repo_url = 'https://github.com/' +  user_name + '/' + repo_name
-    repo_tmpdir = tempdir.name + '/' + repo_name + '.git'
+    repo_tmpdir = tempdir.name + '/' + repo_name
+    repo_data_file = repo_tmpdir + '/' + data_file
     logger.debug('Cloning %s to %s', repo_url, repo_tmpdir)
 
     data_user = get_contribution_data(user_name)
     data_donor = get_contribution_data(donor_name)
     data_repo = diff_contribution_data(data_user, data_donor)
+
+    author_data = user_name.encode() + ' <'.encode() + user_email.encode() + '>'.encode()
+    repo = porcelain.clone(repo_url, repo_tmpdir)
+
+    # TODO: currently only creates on commit per day
+    #       needs to use data_repo[commit_date] 
+    for commit_date in data_repo.keys():
+        commit_stamp = time.mktime(time.strptime(commit_date, '%Y-%m-%d')) + 37334 # magic constant to set the commit time to 9:22:14 AM
+        f = open(repo_data_file, 'w')
+        f.write(commit_date)
+        f.close()
+        porcelain.add(repo, repo_data_file)
+        repo.do_commit(message=commit_date.encode(), committer=author_data, author=author_data, commit_timestamp=commit_stamp)
+
+    r2 = porcelain.push(repo_tmpdir, 'https://' + user_name + ':' + user_pass + '@github.com/' + user_name + '/' + repo_name, 'master')
     
-    #tempdir.cleanup()
+    tempdir.cleanup()
 
 
 if __name__ == '__main__':
